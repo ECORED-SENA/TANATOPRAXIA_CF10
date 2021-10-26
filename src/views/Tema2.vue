ec07d92b4395d0118fac10b8fe0cd76a6cc48cac
john <83730668+DevJohn2021@users.noreply.github.com>
2021-10-26 08:29:51 -0500

    video 2.4

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -349,7 +349,7 @@
 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        <iframe width="699" height="393" src="https://www.youtube.com/embed/VXQJ7DGmkio" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     
 
     p.mb-5 Siendo así, 
