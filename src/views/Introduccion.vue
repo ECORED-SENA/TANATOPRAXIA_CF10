6515940f97d99431987428b894d60d9269326fb0
john <83730668+DevJohn2021@users.noreply.github.com>
2021-10-28 09:09:56 -0500

    cargue video, actividad didactica y material.zip

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -32,7 +32,7 @@
           .col-lg-12
             figure.mb-4
               .video
-                iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+                <iframe width="699" height="393" src="https://www.youtube.com/embed/LF5k803BGPI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 
 
